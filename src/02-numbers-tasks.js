f7254f2aa0d8110ebb8c999adaaf9b009f17b5c3
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-12-11 18:33:57 +0300

    feat: number has finished

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -233,8 +233,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const tmpValue = Number(value);
+  if (tmpValue) {
+    return tmpValue;
+  }
+  return def;
 }
 
 module.exports = {
--- src/test.js
+++
@@ -1,9 +0,0 @@
-//
-// function roundToPowerOfTen(num, pow) {
-//   if (pow === 0) {
-//     return num;
-//   }
-//   console.log(Math.round(num / 10 ** pow) * 10 ** pow);
-// }
-//
-// roundToPowerOfTen(82534834965, 5);
