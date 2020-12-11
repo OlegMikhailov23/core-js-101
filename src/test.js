fea8a44495771cf9924b11f318407d00cb5002c3
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-12-11 18:28:27 +0300

    feat: number in progress

--- src/test.js
+++ src/test.js
@@ -1,9 +1,9 @@
-
-function roundToPowerOfTen(num, pow) {
-  if (pow === 0) {
-    return num;
-  }
-  console.log(Math.round(num / 10 ** pow) * 10 ** pow);
-}
-
-roundToPowerOfTen(82534834965, 5);
+//
+// function roundToPowerOfTen(num, pow) {
+//   if (pow === 0) {
+//     return num;
+//   }
+//   console.log(Math.round(num / 10 ** pow) * 10 ** pow);
+// }
+//
+// roundToPowerOfTen(82534834965, 5);
